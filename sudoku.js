2a86a2b00a35eccc3a09f8acf6b2af9e8feeb8ad
polsan22daw <113674313+polsan22daw@users.noreply.github.com>
2022-10-18 19:47:08 +0200

    Add files via upload

---
+++ sudoku.js
@@ -0,0 +1,7 @@
+window.onload= function(){
+    var td = document.getElementById('rellenar');
+    var FN = document.createElement("input");
+    td.appendChild(FN);
+    FN.setAttribute("type", "text");
+    FN.setAttribute("maxLength", "1");
+}  
