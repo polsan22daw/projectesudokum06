5603db5db07baa74d6bc477b67daa7144a4631a6
polsan22daw <113674313+polsan22daw@users.noreply.github.com>
2022-10-25 19:22:42 +0200

    Delete sudoku.js

--- sudoku.js
+++
@@ -1,7 +0,0 @@
-window.onload= function(){
-    var td = document.getElementById('rellenar');
-    var FN = document.createElement("input");
-    td.appendChild(FN);
-    FN.setAttribute("type", "text");
-    FN.setAttribute("maxLength", "1");
-}  
